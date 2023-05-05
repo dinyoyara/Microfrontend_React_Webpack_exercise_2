a07e9101f78557a186f93743cbc287e6fccced07
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-05 10:21:45 +0300

    Copy Landing and Pricing components from Udemy course.

--- marketing/src/bootstrap.js
+++ marketing/src/bootstrap.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import App from './App';
+
 const mount = (element) => {
-    const content = <h2>Hello world</h2>;
-    ReactDOM.render(content, element);
+    ReactDOM.render(<App />, element);
 };
 
 if (process.env.NODE_ENV === 'development') {
