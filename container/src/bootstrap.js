7b4c65e9edf740feb9790f407082ef77099c2c9b
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-05 18:00:43 +0300

    Fix issue in deploy script.

--- container/src/bootstrap.js
+++ container/src/bootstrap.js
@@ -3,4 +3,5 @@ import ReactDOM from 'react-dom';
 
 import App from './App';
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+const rootElement = document.querySelector('#root');
+ReactDOM.render(<App />, rootElement);
