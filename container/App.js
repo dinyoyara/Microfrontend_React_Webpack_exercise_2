ed08599e99a784f124cc5d89ca60cb8682bb01df
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-05 14:06:19 +0300

    Implement generic integration.

--- container/App.js
+++
@@ -1,4 +0,0 @@
-import React from 'react';
-export default () => {
-    return <h2>Hello World</h2>;
-};
---
+++ container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import MarketingApp from './components/MarketingApp';
+
+export default () => {
+    return (
+        <div>
+            <h2>Hello World</h2>
+            <hr />
+            <MarketingApp />
+        </div>
+    );
+};
