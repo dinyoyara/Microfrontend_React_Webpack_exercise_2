b8313a753fba6931af0175647cd9ffd14c8e7159
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-09 10:02:04 +0300

    Update text again to trigger build action.

--- container/src/App.js
+++ container/src/App.js
@@ -5,7 +5,7 @@ import MarketingApp from './components/MarketingApp';
 export default () => {
     return (
         <div>
-            <h2>Hello World 2</h2>
+            <h2>Hello World</h2>
             <hr />
             <MarketingApp />
         </div>
