163273f1b3fc3e766a49d886676562764de1bf79
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-09 15:06:05 +0300

    Implement Webpack  configs and basic structure in Auth.

---
+++ auth/src/bootstrap.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+
+import App from './App';
+
+const mount = (element, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory || createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(<App history={history} />, element);
+    return {
+        onParentNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    };
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const root = document.querySelector('#_auth_dev_root');
+    if (root) {
+        mount(root, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+export { mount };
